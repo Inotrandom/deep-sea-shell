fd11872c58691fd6b8fe47e1abf1bc0efe07d756
Yevgeny <yacoonan@gmail.com>
2025-05-25 17:25:52 -0400

    refactor: formatting

--- source/utils.h
+++ source/utils.h
@@ -48,76 +48,76 @@ typedef std::vector<size_t> DSSReturnType;
  */
 class DSSDelegate
 {
-    private:
-        std::vector<DSSFunc> connected;
-    
-    public:
-        inline DSSDelegate()
+private:
+    std::vector<DSSFunc> connected;
+
+public:
+    inline DSSDelegate()
+    {
+        this->connected = {};
+    }
+
+    /**
+     * Connect a DSSFunc to the delegate.
+     * 
+     * This will eventually be used in the `call()` function.
+     * Multiple functions can be connected to the same delegate
+     * (this is how a delegate works)
+     * 
+     * @param func The function pointer to connect to the delegate
+     */
+    inline void connect(const DSSFunc func)
+    {
+        if (this == nullptr) {return;}
+
+        this->connected.push_back(func);
+    }
+
+    /**
+     * Attempts to disconnect a DSSFunc from the delegate, if it exists.
+     * 
+     * Will return if `func` does not exist.
+     * 
+     * @param func The function pointer to attempt to disconnect from the delegate.
+     */
+    inline void disconnect(const DSSFunc func)
+    {
+        if (this == nullptr) {return;}
+
+        const auto found = std::find(this->connected.begin(), this->connected.end(), func);
+        
+        if (found == this->connected.end()) {return;}
+        this->connected.erase(found);
+    }
+
+    /**
+     * Calls the delegate.
+     * 
+     * This will pass a copy of the arguments to every single function connected
+     * to the delegate. Additionally, this will execute every single function
+     * (in the order that they were connected- the most recently connected
+     * will be executed last.)
+     * 
+     * @param args A vector of string arguments to be passed into every connected function
+     * 
+     * @return A vector of integers representing status from each function, in corresponding order
+     * to the `connected` member. Note: this vector will be empty if this function errors. Undefined
+     * behavior will not appear in the vector, and as such, relying on the fact that the number
+     * of returns will be consistent is unsafe.
+     */
+    inline auto call(DSSFuncArgs args) -> DSSReturnType
+    {
+        DSSReturnType res = {};
+
+        if (this == nullptr) {return res;}
+
+        for (auto func : this->connected)
         {
-            this->connected = {};
+            res.push_back(func(args));
         }
 
-        /**
-         * Connect a DSSFunc to the delegate.
-         * 
-         * This will eventually be used in the `call()` function.
-         * Multiple functions can be connected to the same delegate
-         * (this is how a delegate works)
-         * 
-         * @param func The function pointer to connect to the delegate
-         */
-        inline void connect(const DSSFunc func)
-        {
-            if (this == nullptr) {return;}
-
-            this->connected.push_back(func);
-        }
-
-        /**
-         * Attempts to disconnect a DSSFunc from the delegate, if it exists.
-         * 
-         * Will return if `func` does not exist.
-         * 
-         * @param func The function pointer to attempt to disconnect from the delegate.
-         */
-        inline void disconnect(const DSSFunc func)
-        {
-            if (this == nullptr) {return;}
-
-            const auto found = std::find(this->connected.begin(), this->connected.end(), func);
-            
-            if (found == this->connected.end()) {return;}
-            this->connected.erase(found);
-        }
-
-        /**
-         * Calls the delegate.
-         * 
-         * This will pass a copy of the arguments to every single function connected
-         * to the delegate. Additionally, this will execute every single function
-         * (in the order that they were connected- the most recently connected
-         * will be executed last.)
-         * 
-         * @param args A vector of string arguments to be passed into every connected function
-         * 
-         * @return A vector of integers representing status from each function, in corresponding order
-         * to the `connected` member. Note: this vector will be empty if this function errors. Undefined
-         * behavior will not appear in the vector, and as such, relying on the fact that the number
-         * of returns will be consistent is unsafe.
-         */
-        inline auto call(DSSFuncArgs args) -> DSSReturnType
-        {
-            DSSReturnType res = {};
-
-            if (this == nullptr) {return res;}
-
-            for (auto func : this->connected)
-            {
-                res.push_back(func(args));
-            }
-
-            return res;
-        }
+        return res;
+    }
 };
 
 /**
@@ -129,40 +129,40 @@ class DSSDelegate
  */
 class DSSCommand
 {
-    private:
-        DSSDelegate delegate;
-        std::string name;
-        std::string description;
-    
-    public:
-        inline DSSCommand(std::string name, std::string description)
-        {
-            this->delegate = DSSDelegate();
-            this->name = name;
-            this->description = description;
-        }
+private:
+    DSSDelegate delegate;
+    std::string name;
+    std::string description;
 
-        /**
-         * Lazily attempts to run this command if the keyword (first token)
-         * matches the command's "name"
-         * 
-         * @param inp The string command to be executed (should contain arguments)
-         * @param delim The separation between individual tokens in a command.
-         * 
-         * @return The result of calling the delegate associated with this command.
-         * Will return an empty vector if an error has occured;
-         * 
-         * @see DSSDelegate::call
-         */
-        inline auto parse_and_exec(std::string inp, std::string delim) -> DSSReturnType
-        {
-            DSSReturnType res = {};
-            
-            if (this == nullptr) {return res;}
+public:
+    inline DSSCommand(std::string name, std::string description)
+    {
+        this->delegate = DSSDelegate();
+        this->name = name;
+        this->description = description;
+    }
 
-            std::vector<std::string> args = string_split(inp, delim);
-            args.erase(args.begin());
+    /**
+     * Lazily attempts to run this command if the keyword (first token)
+     * matches the command's "name"
+     * 
+     * @param inp The string command to be executed (should contain arguments)
+     * @param delim The separation between individual tokens in a command.
+     * 
+     * @return The result of calling the delegate associated with this command.
+     * Will return an empty vector if an error has occured;
+     * 
+     * @see DSSDelegate::call
+     */
+    inline auto parse_and_exec(std::string inp, std::string delim) -> DSSReturnType
+    {
+        DSSReturnType res = {};
+        
+        if (this == nullptr) {return res;}
 
-            res = this->delegate.call(args);
-        }
+        std::vector<std::string> args = string_split(inp, delim);
+        args.erase(args.begin());
+
+        res = this->delegate.call(args);
+    }
 };
