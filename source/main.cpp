e712e90f94e874876887a63fe55906ef5d4d879d
Yevgeny <yacoonan@gmail.com>
2025-05-25 10:32:10 -0400

    docs: main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,17 @@
+/**
+ * DSS is a high-level data-oriented operative programming language. Among others,
+ * its primary purpose is for communication and control of robotic systems.
+ * DSS is intended to exist as both an effective means of storing, parsing, and
+ * using data, but also as a simple tool for operating systems.
+ * 
+ * DSS is designed not just for human users, but also for systems to universally
+ * send/recieve data packets on a network.
+ * 
+ * Grafting to DSS is a core language feature; producing your own organized commands
+ * and namespaces is important to ensure the effectiveness of the language for your
+ * specific needs.
+ */
+
 #include <iostream>
 
 int main(int argv, char** argc)
